dcabbac7e9bb860906067090efab998ada8bdfd2
Emir <emiraksu1608@gmail.com>
2022-09-16 12:01:43 +0300

    Yeniden Canlanma / Harita Düzenlemesi / Tork Ayarı /

--- Snow-Board/Assets/Collision.cs
+++ Snow-Board/Assets/Collision.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Collision : MonoBehaviour
 {
      
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "head")
+        if (other.tag == "Ground")
         {
+            SceneManager.LoadScene(0);
             Debug.Log("Öldün");
         } 
     }
